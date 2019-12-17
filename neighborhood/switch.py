0c2a1e4e4b97fbd5ff203e86b5469cf01425cfd3
Filippo Barba <filippo.barba@protonmail.com>
2019-12-17 15:18:35 +0100

    fixed switch using complete adj matrix

--- neighborhood/switch.py
+++ neighborhood/switch.py
@@ -7,8 +7,8 @@ def switch_exams(enc, index_pair):
     for pair in list(index_pair):
         if solution[pair[0]] != solution[pair[1]]:
             # print(enc[pair[0], :], d[pair[1]])
-            p = (enc[pair[0], :] == solution[pair[1]])
-            p1 = (enc[pair[1], :] == solution[pair[0]])
+            p = (np.delete(enc[pair[0], :],pair[1]) == solution[pair[1]])
+            p1 = (np.delete(enc[pair[1], :],pair[0]) == solution[pair[0]])
             if any(p) == False and any(p1) == False:
                 # list of feasible neighbour solution
                 tmp_solution = solution.copy()
