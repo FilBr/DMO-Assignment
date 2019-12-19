746f9e7fdf3336187309049002320eacd0ad103f
Filippo Barba <filippo.barba@protonmail.com>
2019-12-19 12:43:17 +0100

    new strategy for obj_func calculation

--- main.py
+++ main.py
@@ -75,13 +75,13 @@ if __name__ == "__main__":
             for exam in color_dict:
                 solution_file.write(f"{exam + 1} {color_dict[exam] + 1}\n")
 
-        initial_solution = Solution(first_sol, adj_mat, max_col, num_students)
+        initial_solution = Solution(first_sol, adj_mat, max_col, num_students, initial=True)
         #initial_solution.get_penalty()
         #initial_solution.avg_neighbourhood_penalty()
         #neighbour = initial_solution.get_random_neighbour()
         #neighbour.get_penalty()
 
-        simulated_annealing = Simulated_annealing(100000, initial_solution)
+        simulated_annealing = Simulated_annealing(100, initial_solution)
         simulated_annealing.run()
 
         # encoding_matrix, distance_matrix = encoding(adj_mat, color_dict)
